74da1af475aa904313030ec68c3c3c4002308520
Julia Grifiths Allison <griffithsallison0615@gmail.com>
2022-10-26 10:29:40 -0400

    first commit, created template and git ignore

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pokemon_Random_Battle_Sim.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
